8c5c43ecad80df71eed04e2bcbef908dadb09e20
Gleb Rodionov <137335455+ss3nclam@users.noreply.github.com>
2024-04-26 23:39:31 +0400

    upd

--- installer.sh
+++ installer.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
-echo $'\n# Setting for mplc4-cleaner\nroot ALL=(ALL) ALL\nuser ALL=(ALL) NOPASSWD: /bin/systemctl daemon-reload\nuser ALL=(ALL) NOPASSWD: /bin/systemctl * mplc4\nuser ALL=(ALL) NOPASSWD: /bin/systemctl * mplc4-cleaner\n' | sudo tee -a /etc/sudoers
-chmod +x /opt/mplc4/log/cleaner.py
-echo $'[Unit]\nDescription=mplc4-cleaner\n\n[Service]\nExecStart=/opt/mplc4-cleaner/cleaner.py\n\n[Install]\nWantedBy=multi-user.target\n' | sudo tee -a /lib/systemd/system/mplc4-cleaner.service
-systemctl daemon-reload
-systemctl enable mplc4-cleaner.service
-systemctl start mplc4-cleaner.service
+echo $'\n# For mplc4-logs-cleaner\nroot ALL=(ALL) ALL\nuser ALL=(ALL) NOPASSWD: /bin/systemctl daemon-reload\nuser ALL=(ALL) NOPASSWD: /bin/systemctl * mplc4\nuser ALL=(ALL) NOPASSWD: /bin/systemctl * mplc4-logs-cleaner\n' | sudo tee -a /etc/sudoers
+sudo chmod +x /opt/mplc4-cleaner/cleaner.py
+echo $'[Unit]\nDescription=mplc4-logs-cleaner\n\n[Service]\nExecStart=/opt/mplc4_logs_cleaner/cleaner.py\n\n[Install]\nWantedBy=multi-user.target\n' | sudo tee -a /lib/systemd/system/mplc4-logs-cleaner.service
+sudo systemctl daemon-reload
+sudo systemctl enable mplc4-logs-cleaner.service
+sudo systemctl start mplc4-logs-cleaner.service
